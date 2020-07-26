cc8ebe8559b078e97cc90eb9cd62326dc64bce33
vivek17th@gmail.com <vivek17th@gmail.com>
2020-07-26 14:00:59 +0530

    first commit for jar project by vivek

---
+++ PintJar/src/com/vivek/print/VivekPrint.java
@@ -0,0 +1,14 @@
+package com.vivek.print;
+
+public class VivekPrint {
+VivekPrint(){
+	System.out.println("vivek print construct called");
+}
+public static String vivekMethod(){
+	
+	return "Hi Vivek";
+}
+	public static void main(String arg[]){
+		VivekPrint vk=new VivekPrint();
+	}
+}
---
+++ vivekjar/src/com/vivek/jar/Vivekjar.java
@@ -0,0 +1,22 @@
+package com.vivek.jar;
+
+import javax.swing.*;    
+public class Vivekjar{    
+	Vivekjar(){    
+JFrame f=new JFrame();    
+                    
+JButton b=new JButton("click");    
+b.setBounds(130,100,100, 40);    
+        
+f.add(b);    
+            
+f.setSize(300,400);    
+f.setLayout(null);    
+f.setVisible(true);    
+            
+f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    
+}    
+public static void main(String[] args) {    
+    new Vivekjar();    
+}    
+}    
